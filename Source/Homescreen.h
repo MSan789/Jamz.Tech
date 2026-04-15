c8dc636ad478693a260e1676b87ac0cdd7460236
Tanishi29 <tkumar19@asu.edu>
2026-04-15 14:12:48 -0700

    Refactor Homescreen class and add ClusterMapComponent

--- Source/Homescreen.h
+++ Source/Homescreen.h
@@ -3,42 +3,45 @@
 #include "AudioCards.h"
 #include "LocalDatabase.h"
 #include "HeaderBar.h"
+#include "ClusterMapComponent.h"
 #include <vector>
 
 class Homescreen : public juce::Component {
-public:
+    public:
     Homescreen();
     ~Homescreen() override;
-
+    
     void paint(juce::Graphics&) override;
     void resized() override;
-
+    
     void setUsername(const juce::String& username);
     void setRole(const juce::String& newRole);
 
     void loadRecordings();
+    
+    
 
     juce::String currentRole;
     std::function<void()> onLogoutClicked;
     std::function<void()> onRecordClicked;
-    std::function<void(int)> onEditClicked; // Pass the index of the audio card to edit
-    std::function<void(const RecordingEntry&)> onEditRequested; // New callback for edit requests, passing the index of the audio card
-
-private:
+    std::function<void(int)> onEditClicked; 
+    std::function<void(const RecordingEntry&)> onEditRequested; 
+    
+    private:
     HeaderBar headerBar;
 
     juce::String currUser;
-
+    
     //top of the homescreen
-
+    
     juce::Label greetingLabel;
     juce::Label appName;
     juce::TextEditor searchBar;
-
+    
     //sidebar
     juce::Component categoriesPanel;
     juce::Label categoriesTitle;
-
+       
     LocalDatabase database;
     std::vector<RecordingEntry> recordings;
     std::vector<std::unique_ptr<AudioCards>> audioCards;
@@ -48,8 +51,14 @@ private:
     juce::AudioFormatManager formatManager;
     juce::AudioTransportSource transportSource;
     juce::Viewport viewport;
+    juce::Viewport mapViewport;
     juce::Component cardsContainer;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    
+    ClusterMapComponent clusterMap;
+    juce::TextButton listViewTab { "List" };
+    juce::TextButton mapViewTab { "Map" };
+    bool showingMap = false;
 
     void playRecording(const RecordingEntry& entry);;
 
